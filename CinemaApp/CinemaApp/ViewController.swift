ea2453430616b095b1436ed739b5ff33336b209d
İsmail AÇIKYÜREK <82399051+ismailacikyurek@users.noreply.github.com>
2022-08-02 00:03:56 +0300

    project init

---
+++ CinemaApp/CinemaApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CinemaApp
+//
+//  Created by İSMAİL AÇIKYÜREK on 2.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
